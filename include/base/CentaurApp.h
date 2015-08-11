5e1303eba1a26d7a96d918b14d3df38005f964c7
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-08-11 15:30:44 -0600

    Update to new constructors

--- include/base/CentaurApp.h
+++ include/base/CentaurApp.h
@@ -11,7 +11,7 @@ InputParameters validParams<CentaurApp>();
 class CentaurApp : public MooseApp
 {
 public:
-  CentaurApp(const std::string & name, InputParameters parameters);
+  CentaurApp(InputParameters parameters);
   virtual ~CentaurApp();
 
   static void registerApps();
